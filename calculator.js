877f249711d86dca478a77a533031e57e8a6b4ae
Odera5 <anikingsleyodera5@gmail.com>
2024-11-03 19:07:34 +0100

    my commit

--- calculator.js
+++ calculator.js
@@ -1,6 +1,6 @@
 //step1
 function add(a, b) {
-    return a + b;
+    return Number(a) + Number(b);
 }
 function subtract(a, b) {
     return a - b;
@@ -21,142 +21,58 @@ let result;
 
 //step3
 function operate(operator, a, b) {
-
     if (operator === "+") {
         return add(a, b);
-    }
-
-    else if (operator === "-") {
+    } else if (operator === "-") {
         return subtract(a, b);
-
-    }
-    else if (operator === "*") {
-        return multiply(a, b)
-    }
-    else {
-        return divide(a, b)
+    } else if (operator === "*") {
+        return multiply(a, b);
+    } else {
+        return divide(a, b);
     }
 }
-
-
-
 
 //step 5
 
 let display = [];
 
-const one = document.querySelector("#one");
-const input = document.querySelector("input")
-const two = document.querySelector("#two");
-const three = document.querySelector("#three");
-const four = document.querySelector("#four");
-const five = document.querySelector("#five");
-const six = document.querySelector("#six");
-const seven = document.querySelector("#seven");
-const eight = document.querySelector("#eight");
-const nine = document.querySelector("#nine");
-const zero = document.querySelector("#zero");
-const sum = document.querySelector("#sum")
-const minus = document.querySelector("#minus")
-const times = document.querySelector("#times")
-const division = document.querySelector("#division")
-const equal = document.querySelector("#equal")
-const clear = document.querySelector("#clear")
+const input = document.querySelector("input");
+const equal = document.querySelector("#equal");
+const clear = document.querySelector("#clear");
+
+const numberBtns = document.querySelectorAll(".numbers");
+const operatorBtns = document.querySelectorAll(".operators");
 
 
+numberBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        //console.log(operator);
 
-one.addEventListener("click", () => {
+        if (!operator) {
+            firstNumber += btn.textContent;
+            input.value = firstNumber;
+        } else {
+            secondNumber += btn.textContent;
+            input.value = secondNumber;
+        }
+    });
+});
 
-
-    display.push(1);
-    input.value = display.join('');
+operatorBtns.forEach((btn) => {
+    btn.addEventListener("click", () => operator = btn.textContent)
 })
-
-two.addEventListener("click", () => {
-    display.push(2);
-    input.value = display.join('');
-})
-
-three.addEventListener("click", () => {
-    display.push(3);
-    input.value = display.join('');
-})
-
-four.addEventListener("click", () => {
-    display.push(4);
-    input.value = display.join('');
-})
-
-five.addEventListener("click", () => {
-    display.push(5);
-    input.value = display.join('');
-})
-
-six.addEventListener("click", () => {
-    display.push(6);
-    input.value = display.join('');
-})
-
-seven.addEventListener("click", () => {
-    display.push(7);
-    input.value = display.join('');
-})
-
-eight.addEventListener("click", () => {
-    display.push(8);
-    input.value = display.join('');
-})
-
-nine.addEventListener("click", () => {
-
-    display.push(9);
-    input.value = display.join('');
-})
-
-zero.addEventListener("click", () => {
-    display.push(0);
-    input.value = display.join('');
-
-})
-
-
-
-sum.addEventListener("click", () => {
-    firstNumber = display.pop();
-    display.push("+");
-    operator = display.pop();
-
-})
-
-minus.addEventListener("click", () => {
-    firstNumber = display.pop();
-    display.push("-");
-    operator = display.pop();
-})
-
-times.addEventListener("click", () => {
-    firstNumber = display.pop();
-    display.push("*");
-    operator = display.pop();
-})
-
-division.addEventListener("click", () => {
-    firstNumber = display.pop();
-    display.push("/");
-    operator = display.pop();
-})
-
-
 
 equal.addEventListener("click", () => {
-    secondNumber = display.pop();
     result = operate(operator, firstNumber, secondNumber);
     input.value = result;
-})
+    firstNumber = result;
+    secondNumber = "";
+});
 
 clear.addEventListener("click", () => {
-    display.pop();
 
+    firstNumber = "";
+    secondNumber = "";
+    operator = "";
     input.value = "";
-})
-
+});
